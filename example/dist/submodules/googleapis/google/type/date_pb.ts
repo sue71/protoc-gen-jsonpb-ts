f0b35a715457795da16884713e06a7301dd5a41e
sue71 <s.masaki07@gmail.com>
2018-09-14 13:59:55 +0900

    fix: fixed filter condition for dependency list

---
+++ example/dist/submodules/googleapis/google/type/date_pb.ts
@@ -0,0 +1,11 @@
+/* GENERATED FROM submodules/googleapis/google/type/date.proto. DO NOT EDIT MANUALLY. */
+/* tslint:disabled */
+/* eslint-disable */
+
+export namespace google.type {
+  export interface Date {
+    year?: number;
+    month?: number;
+    day?: number;
+  }
+}
